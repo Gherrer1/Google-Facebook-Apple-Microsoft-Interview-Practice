04bf6016abe67e72aecd8146785a77c9617f17dc
Gerardo <gherrer1@binghamton.edu>
2019-01-01 12:34:15 -0500

    dfs solution to friend_circles.py

--- unionFind/friend_circles.py
+++ unionFind/friend_circles.py
@@ -1,4 +1,4 @@
-# solution 1
+# solution 1 - union find
 def find(parents, x):
     temp = x
     while parents[temp] != temp:
@@ -27,4 +27,21 @@ def find_circle_num(M):
         circle = find(parents, i)
         circles_set[circle] = True
     return len(circles_set)
-    
+
+# solution 2 - dfs
+def dfs(visited, index, M):
+    visited[index] = 1
+    friends = M[index]
+    for i, val in enumerate(friends):
+        if val == 1 and visited[i] == 0:
+            dfs(visited, i, M)
+
+def find_circle_num2(M):
+    num_students = len(M)
+    visited = [0 for i in range(num_students)]
+    circle_count = 0
+    for i in range(num_students):
+        if visited[i] == 0:
+            dfs(visited, i, M)
+            circle_count += 1
+    return circle_count
